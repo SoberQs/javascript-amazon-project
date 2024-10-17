d7e21c0c091b70484afe86b4d0bb85145f6cd3bb
Soberqs <qiuyidong0107@163.com>
2024-10-17 13:08:20 +0100

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -84,6 +84,15 @@ document.querySelectorAll('.js-add-to-cart')
         });
       }
 
-      console.log(cart);
+      // calculate total quantity of the cart
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
     });
   });
+
